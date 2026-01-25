08e422186e3842687851ca5e7cac8556d5c2a609
Yanu <59870900+khusanova@users.noreply.github.com>
2026-01-25 13:01:35 +0100

    Add congratulation message on pattern completion

--- Knitty/ContentView.swift
+++ Knitty/ContentView.swift
@@ -8,41 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rowNumber = 0
+    @State private var rowNumber = 110
     @State private var currentPattern = Pattern.bananaSock
     var currentRow: [RowElement] { currentPattern.displayRow(at: rowNumber)}
     var body: some View {
-        VStack {
-            Text("🧶 Knit!!!").font(.largeTitle)
-            Text("You are at the row \(rowNumber)")
-            HStack{
-                Text("Follow this pattern: ")
-            }
-            ScrollView(.horizontal) {
-                  HStack {
-                      ForEach(0..<currentRow.count){ i in
-                          VStack{
-                              Text("\(currentRow[i].number)")
-                              
-                              Text("\(currentRow[i].abbreviation)")
+        if rowNumber < currentPattern.count{
+            VStack {
+                Text("🧶 Knit!!!").font(.largeTitle)
+                Text("You are at the row \(rowNumber)")
+                HStack{
+                    Text("Follow this pattern: \(currentPattern.count)")
+                }
+                ScrollView(.horizontal) {
+                      HStack {
+                          ForEach(0..<10){ i in
+                              VStack{
+                                  Text("\(currentRow[i].number)")
+                                  
+                                  Text("\(currentRow[i].abbreviation)")
+                              }
                           }
                       }
                   }
-              }
-            .background(Color.mint)
-            
-            HStack {
-                  Button("-1") {
-                      rowNumber -= 1
+                .background(Color.mint)
+                
+                HStack {
+                      Button("-1") {
+                          rowNumber -= 1
+                      }
+                                                            
+                      Button("+1") {
+                          rowNumber += 1
+                      }
                   }
-                                                        
-                  Button("+1") {
-                      rowNumber += 1
-                  }
-              }
+            }
+            .padding()
         }
-        .padding()
-    }
+        else if rowNumber == currentPattern.count{
+            Text("Congratulations!")
+            Button("Start again"){
+                rowNumber = 0
+            }
+        }
+        }
+        
 }
 
 #Preview {
