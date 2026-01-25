366b07ac5ca3e57b1aca1e0ee77b97dcd9f3b2ae
Yanu <59870900+khusanova@users.noreply.github.com>
2026-01-25 16:02:01 +0100

    Refactor generation of example patterns after refactoring Row

--- Knitty/Pattern.swift
+++ Knitty/Pattern.swift
@@ -46,14 +46,14 @@ struct Row: Identifiable, Codable {
         return elements.count
     }
     
-    init(fullRowPattern: [String], rowExtras: [Int: String] = [:]) {
+    init(stitches: [String], rowExtras: [Int: String] = [:]) {
         var rowElements: [RowElement] = []
         
-        for i in 1...fullRowPattern.count {
-            rowElements.append(RowElement(number: "\(i)", abbreviation: fullRowPattern[i-1]))
+        for i in 1...stitches.count {
+            rowElements.append(RowElement(number: "\(i)", abbreviation: stitches[i-1]))
         }
         
-        if Self.checkExtrasIndexes(rowExtras, count: fullRowPattern.count) {
+        if Self.checkExtrasIndexes(rowExtras, count: stitches.count) {
             let sortedExtras = rowExtras.sorted {$0.key > $1.key}
             for (position, abbreviation) in sortedExtras{
                 let elementType = RowElementType(from: abbreviation)
@@ -81,7 +81,7 @@ struct Row: Identifiable, Codable {
         self.elements += row.elements
     }
     
-    static func generateFullRowPattern(basePattern: [String], n: Int) -> [String] {
+    static func generateStitches(basePattern: [String], n: Int) -> [String] {
         var result: [String] = []
         let length = basePattern.count
         
@@ -164,18 +164,18 @@ extension Row{
 }
 
 extension Pattern{
-    static let example = Pattern(baseRow: Row(basePattern: Row.ribbing2x2, n: 42), length: 47, name: "Example Pattern")
-    static let bananaSockTop = Pattern(baseRow: Row(basePattern: Row.ribbing2x2, n: 56, rowExtras: Dictionary(uniqueKeysWithValues: (1...4).map{ ($0*14, "end of needle")})), length: 15, name: "Banana Sock", patternURL: URL(filePath: "https://www.kotona.com/articles/banana-socks"))
-    static let bananaSockKnitRow = Row(basePattern: Row.ribbing2x2, n: 14, rowExtras: [14: "end of needle"]) +
-        Row(basePattern: Row.knit, n: 26, rowExtras: [14: "end of needle", 26: "end of needle"]) +
-        Row(basePattern: Row.ribbing2x2, n: 16, rowExtras: [16: "end of needle"])
-    static let bananaSockPurlRow = Row(basePattern: Row.ribbing2x2, n: 14, rowExtras: [14: "end of needle"]) +
-        Row(basePattern: Row.purl, n: 26, rowExtras: [14: "end of needle", 26: "end of needle"]) +
-        Row(basePattern: Row.ribbing2x2, n: 16, rowExtras: [16: "end of needle"])
+    static let example = Pattern(baseRow: Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 42)), length: 47, name: "Example Pattern")
+    static let bananaSockTop = Pattern(baseRow: Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 56), rowExtras: Dictionary(uniqueKeysWithValues: (1...4).map{ ($0*14, "end of needle")})), length: 15, name: "Banana Sock", patternURL: URL(filePath: "https://www.kotona.com/articles/banana-socks"))
+    static let bananaSockKnitRow = Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 14), rowExtras: [14: "end of needle"]) +
+    Row(stitches: Row.generateStitches(basePattern: Row.knit, n: 26), rowExtras: [14: "end of needle", 26: "end of needle"]) +
+    Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 16), rowExtras: [16: "end of needle"])
+    static let bananaSockPurlRow = Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 14), rowExtras: [14: "end of needle"]) +
+    Row(stitches: Row.generateStitches(basePattern: Row.purl, n: 26), rowExtras: [14: "end of needle", 26: "end of needle"]) +
+    Row(stitches: Row.generateStitches(basePattern: Row.ribbing2x2, n: 16), rowExtras: [16: "end of needle"])
     static let bananaSockBody = (0..<10).map{ _ in Pattern(baseRow: Pattern.bananaSockKnitRow, length: 5) + Pattern(baseRow: Pattern.bananaSockPurlRow, length: 5)}.reduce(bananaSockTop,+)
-    static let rowDecreases = (7...14).reversed().map {Row(basePattern: Row.knit, n: $0, rowExtras: [$0-3: "k2tog"]) +
-        Row(basePattern: Row.knit, n: $0, rowExtras: [2: "skp"]) +
-        Row(basePattern: Row.knit, n: $0, rowExtras: [$0-3: "k2tog"]) +
-        Row(basePattern: Row.knit, n: $0, rowExtras: [2: "skp"])}
+    static let rowDecreases = (7...14).reversed().map {Row(stitches: Row.generateStitches(basePattern: Row.knit, n: $0), rowExtras: [$0-3: "k2tog"]) +
+        Row(stitches: Row.generateStitches(basePattern: Row.knit, n: $0), rowExtras: [2: "skp"]) +
+        Row(stitches: Row.generateStitches(basePattern: Row.knit, n: $0), rowExtras: [$0-3: "k2tog"]) +
+        Row(stitches: Row.generateStitches(basePattern: Row.knit, n: $0), rowExtras: [2: "skp"])}
     static let bananaSock = Pattern.bananaSockBody + Pattern(rows: rowDecreases)
 }
