9cf319dbb15f598e4601c9521887085969e08990
Yanu <59870900+khusanova@users.noreply.github.com>
2026-01-31 20:19:39 +0100

    Add totalRowCounter to ProjectPart

--- Knitty/Models/Project.swift
+++ Knitty/Models/Project.swift
@@ -10,15 +10,22 @@ import Foundation
 struct ProjectPart: Codable, Identifiable{
     var id = UUID()
     var name: String
-    var patterns: [UUID]
+    var patternOrder: [UUID]
     var patternRowCounters: [Int]
-    var totalRowCount: Int = 0
+    var totalRowCounter: Int {
+        patternRowCounters.reduce(0, +)
+    }
 }
 
 struct Project: Codable, Identifiable{
     var id = UUID()
     var name: String
     var projectParts: [ProjectPart]
+    var patterns: [UUID: Pattern]
     var currentProjectPart: UUID?
     var description: String?
+    
+    func totalRowCount(projectPart: ProjectPart) -> Int {
+        projectPart.patternOrder.compactMap { patterns[$0]?.count ?? 0}.reduce(0,+)
+    }
 }
