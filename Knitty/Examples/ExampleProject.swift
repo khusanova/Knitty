79bbfc5aebb62bf0f5ec9916bd485429422c8db4
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-06 18:57:06 +0100

    Update Project Example after refactoring Project
    Old initiation of Project Example is incompatible with new Project structure (ProjectPart was moved to Project)

--- Knitty/Examples/ExampleProject.swift
+++ Knitty/Examples/ExampleProject.swift
@@ -7,17 +7,7 @@
 
 
 extension Project{
-    static let bananaSockRightPatternIDs = [Pattern.bananaSockTop.id]
-    + (0..<10).map {_ in Pattern.bananaSockRightBody.id}
-    + [Pattern.bananaSockRightBottom.id]
-    static let bananaSockRight = ProjectPart(name: "Right banana sock", patternOrder: bananaSockRightPatternIDs)
-    static let bananaSockLeftPatternIDs = [Pattern.bananaSockTop.id]
-    + (0..<10).map {_ in Pattern.bananaSockLeftBody.id}
-    + [Pattern.bananaSockLeftBottom.id]
-    static let bananaSockLeft = ProjectPart(name: "Left banana sock", patternOrder: bananaSockLeftPatternIDs)
-    static let patternsForBananaSock = [Pattern.bananaSockTop.id: Pattern.bananaSockTop, Pattern.bananaSockRightBody.id: Pattern.bananaSockRightBody,
-                                        Pattern.bananaSockLeftBody.id: Pattern.bananaSockLeftBody,
-                                        Pattern.bananaSockRightBottom.id: Pattern.bananaSockRightBottom,
-                                        Pattern.bananaSockLeftBottom.id: Pattern.bananaSockLeftBottom]
-    static let bananaSocks = Project(name: "Banana socks", projectParts: [bananaSockRight, bananaSockLeft], patterns: patternsForBananaSock)
+    static let bananaSockRight = ("Right sock", [Pattern.bananaSockTop] + (0..<10).map {_ in Pattern.bananaSockRightBody} + [Pattern.bananaSockRightBottom])
+    static let bananaSockLeft = ("Left sock", [Pattern.bananaSockTop] + (0..<10).map {_ in Pattern.bananaSockLeftBody} + [Pattern.bananaSockLeftBottom])
+    static let bananaSocks = Project(name: "Banana socks", projectParts: [bananaSockRight, bananaSockLeft])
 }
