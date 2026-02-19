664975c7ae5f94f6d0aa5b4075e5d8ba7b5f199f
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-19 18:00:45 +0100

    Make rowCounter optional attribute in Pattern
    When project is created and the user did not start knitting, rowCounter is not necessary

--- Knitty/Models/Pattern.swift
+++ Knitty/Models/Pattern.swift
@@ -117,7 +117,7 @@ struct Pattern: Identifiable, Codable {
     var id = UUID()
     var rows: [UUID: Row]
     var rowOrder: [UUID]
-    var rowCounter: Int
+    var rowCounter: Int?
     var name: String?
     var details: String?
     var count: Int {
@@ -128,7 +128,6 @@ struct Pattern: Identifiable, Codable {
         self.rowOrder = rows.map { $0.id }
         self.rows = Dictionary(rows.map { ($0.id, $0) },
                                uniquingKeysWith: { old, _ in old})
-        self.rowCounter = 0
         self.name = name
         self.details = details
     }
@@ -136,7 +135,6 @@ struct Pattern: Identifiable, Codable {
     init(baseRow: Row, length: Int, name: String? = nil, details: String? = nil) {
         self.rows = [baseRow.id: baseRow]
         self.rowOrder = (0..<length).map { _ in baseRow.id}
-        self.rowCounter = 0
         self.name = name
         self.details = details
     }
@@ -152,7 +150,10 @@ struct Pattern: Identifiable, Codable {
     }
     
     func getCurrentRow() -> Row? {
-        getRow(at: rowCounter)
+        guard let index = rowCounter else {
+            return nil
+        }
+        return getRow(at: index)
     }
     
     mutating func updateRow(at index: Int, newRow: Row) {
