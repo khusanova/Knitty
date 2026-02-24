390068810d9d1d75a21f3c0e17a271e03fdbb57f
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-24 16:11:42 +0100

    Refactor ProjectView
    Check if viewModel.projectPartIndex has a value using != because the variable projectPartIndex is not used in ProjectView

--- Knitty/Views/ProjectView.swift
+++ Knitty/Views/ProjectView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct ProjectView: View {
     @State var viewModel: ProjectViewModel = ProjectViewModel()
     var body: some View {
-        if let projectPartIndex = viewModel.projectPartIndex {
+        if viewModel.projectPartIndex != nil {
             KnittingView(viewModel: viewModel)
         }
         else{
