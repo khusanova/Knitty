7b60e67b52e5af5e99a1119b623a57a56d518b51
Yanu <59870900+khusanova@users.noreply.github.com>
2026-03-29 15:22:14 +0200

    Add MainView to the start of the app.
    A simple MainView with a welcome message.

---
+++ Knitty/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Knitty
+//
+//  Created by Y. Khusanova on 29.03.26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Welcome to Knitty!")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
